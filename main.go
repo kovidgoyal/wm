ff4145711e20bd57b4347d428a325f329a63b211
Kovid Goyal <kovid@kovidgoyal.net>
2025-07-28 17:00:46 +0530

    Command to set battery charging thresholds

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+	"strconv"
 
 	"github.com/kovidgoyal/kitty/tools/cli"
 	"github.com/kovidgoyal/kitty/tools/utils"
@@ -120,6 +122,46 @@ func main() {
 			}
 			if err = hypr.SuperTab(); err != nil {
 				rc = 1
+			}
+			return
+		},
+	})
+	const BELOW_DEFAULT = 0
+	const ABOVE_DEFAULT = 100
+	root.AddSubCommand(&cli.Command{
+		Name:             "charge-control",
+		ShortDescription: "Set thresholds to control when the battery charges",
+		Usage:            "start-charging-below stop-charging-above",
+		HelpText:         fmt.Sprintf("Set percentage levels to control when the battery starts and stops charging. Default when unspecified is to not restrict charging, which means below is set to %d and above to %d. Specify the percentages as integers between 0 and 100.", BELOW_DEFAULT, ABOVE_DEFAULT),
+		OnlyArgsAllowed:  true,
+		Run: func(cmd *cli.Command, args []string) (rc int, err error) {
+			below, above := BELOW_DEFAULT, ABOVE_DEFAULT
+			if len(args) > 0 {
+				if below, err = strconv.Atoi(args[0]); err != nil {
+					return 1, err
+				}
+			}
+			if len(args) > 1 {
+				if above, err = strconv.Atoi(args[0]); err != nil {
+					return 1, err
+				}
+			}
+			const base = `/sys/class/power_supply`
+			entries, err := os.ReadDir(base)
+			if err != nil {
+				return 1, err
+			}
+			for _, e := range entries {
+				q := filepath.Join(base, e.Name())
+				if _, ex := os.Stat(filepath.Join(q, "charge_control_start_threshold")); ex == nil {
+					for name, val := range map[string]int{
+						"charge_control_start_threshold": below, "charge_control_end_threshold": above} {
+						if err = os.WriteFile(filepath.Join(q, name), []byte(strconv.Itoa(val)), 0644); err != nil {
+							return 1, err
+						}
+					}
+
+				}
 			}
 			return
 		},
